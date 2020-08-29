d690b3cc55ee3d3b22edb4d7316face9cc40b37e
hitendrasinh-Parmar <hitrajput098@gmail.com>
2020-08-29 10:34:47 +0530

    Updates

--- precache-manifest.36982a3333d6ddee4eb24cde2054b134.js
+++ precache-manifest.36982a3333d6ddee4eb24cde2054b134.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce15193795efb793ef3d188ce701e024",
+    "revision": "197b3b4693e156514bda99533ff18049",
     "url": "/your-chef/index.html"
   },
   {
-    "revision": "67e6630a406228d82d32",
-    "url": "/your-chef/static/css/main.3be44d43.chunk.css"
+    "revision": "e99e4599ea07c7f65da7",
+    "url": "/your-chef/static/css/main.dddf3bc9.chunk.css"
   },
   {
-    "revision": "7bb2835475b25cb2b8c2",
-    "url": "/your-chef/static/js/2.9e00cb96.chunk.js"
+    "revision": "8df72a7762a4312eebbd",
+    "url": "/your-chef/static/js/2.e8d9429e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/your-chef/static/js/2.9e00cb96.chunk.js.LICENSE.txt"
+    "url": "/your-chef/static/js/2.e8d9429e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "67e6630a406228d82d32",
-    "url": "/your-chef/static/js/main.29f96638.chunk.js"
+    "revision": "e99e4599ea07c7f65da7",
+    "url": "/your-chef/static/js/main.f66ed017.chunk.js"
   },
   {
     "revision": "18b895498a971a1d8760",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/your-chef/precache-manifest.dd2c0b0fc10d999087d2815f12590fb8.js"
+  "/your-chef/precache-manifest.36982a3333d6ddee4eb24cde2054b134.js"
 );
 
 self.addEventListener('message', (event) => {
